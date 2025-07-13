d85b79cbea65ba437c784ab4531002a1241875df
nauvalachid <nauvalachid@gmail.com>
2025-07-14 04:44:36 +0700

    tambahkan dan konsolidasikan palet warna ke dalam AppColors

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = const Color(0xFF1976D2);
+
+  static const Color secondary = Color(0xff6199f6);
+
+  static const Color card1 = Color(0xFF2980B9);
+
+  static const Color card2 = Color.fromARGB(255, 181, 223, 255);
+
+  static const Color lightSheet = Color(0xffF5F5F5);
+
+  static const Color blueLight = Color(0xffC7D0EB);
+
+  static const Color black = Color(0xff000000);
+
+  static const Color white = Color(0xffFFFFFF);
+
+  static const Color green = Color(0xff50C474);
+
+  static const Color red = Color(0xffF65151);
+  
+  static const Color redCustom = Color(0xffFF7664);
+
+  static const Color card = Color(0xffE5E5E5);
+
+  static const Color title = Color(0xffEFF0F6);
+
+  static const Color disabled = Color(0xffC8D1E1);
+
+  static const Color subtitle = Color(0xff7890CD);
+
+  static const Color stroke = Color(0xffDBDBDB);
+}
