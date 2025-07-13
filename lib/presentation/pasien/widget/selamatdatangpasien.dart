7b34cfe0147e5a88eb73b3fab635137b69c2a77e
nauvalachid <nauvalachid@gmail.com>
2025-07-14 02:43:34 +0700

    membuat sealamat datang pasien card

---
+++ lib/presentation/pasien/widget/selamatdatangpasien.dart
@@ -0,0 +1,62 @@
+// lib/widget/selamatdatangpasien_card.dart (contoh path)
+import 'package:flutter/material.dart';
+
+class SelamatDatangPasienCard extends StatelessWidget {
+  final String userName; // Tambahkan parameter userName
+
+  const SelamatDatangPasienCard({
+    Key? key,
+    required this.userName, // Jadikan wajib
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+      padding: const EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        gradient: const LinearGradient(
+          colors: [Color(0xFF2980B9), Color.fromARGB(255, 181, 223, 255)],
+          begin: Alignment.centerLeft,
+          end: Alignment.centerRight,
+        ),
+        borderRadius: BorderRadius.circular(15),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 10,
+            offset: Offset(0, 4),
+          ),
+        ],
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            flex: 2,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 3),
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.2),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: const Text(
+                    '#tetapsehattanpacemas',
+                    style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold, color: Colors.white),
+                  ),
+                ),
+                const SizedBox(height: 5),
+                Text( // Gunakan Text widget biasa (tanpa const) karena isinya dinamis
+                  'Selamat Datang, $userName', // Menggunakan userName di sini
+                  style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
