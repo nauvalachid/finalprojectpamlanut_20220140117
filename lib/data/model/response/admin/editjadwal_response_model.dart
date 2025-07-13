b89a8da701efff50890109a3f2aa4fc8560f8949
nauvalachid <nauvalachid@gmail.com>
2025-07-14 04:48:35 +0700

    membuat edit jadwal response model

---
+++ lib/data/model/response/admin/editjadwal_response_model.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+
+class EditJadwalResponseModel {
+    final int? id;
+    final String? namaBidan;
+    final DateTime? tanggal;
+    final String? startTime;
+    final String? endTime;
+    final DateTime? createdAt;
+    final DateTime? updatedAt;
+
+    EditJadwalResponseModel({
+        this.id,
+        this.namaBidan,
+        this.tanggal,
+        this.startTime,
+        this.endTime,
+        this.createdAt,
+        this.updatedAt,
+    });
+
+    factory EditJadwalResponseModel.fromJson(String str) => EditJadwalResponseModel.fromMap(json.decode(str));
+
+    String toJson() => json.encode(toMap());
+
+    factory EditJadwalResponseModel.fromMap(Map<String, dynamic> json) => EditJadwalResponseModel(
+        id: json["id"],
+        namaBidan: json["nama_bidan"],
+        tanggal: json["tanggal"] == null ? null : DateTime.parse(json["tanggal"]),
+        startTime: json["start_time"],
+        endTime: json["end_time"],
+        createdAt: json["created_at"] == null ? null : DateTime.parse(json["created_at"]),
+        updatedAt: json["updated_at"] == null ? null : DateTime.parse(json["updated_at"]),
+    );
+  
+    Map<String, dynamic> toMap() => {
+        "id": id,
+        "nama_bidan": namaBidan,
+        "tanggal": "${tanggal!.year.toString().padLeft(4, '0')}-${tanggal!.month.toString().padLeft(2, '0')}-${tanggal!.day.toString().padLeft(2, '0')}",
+        "start_time": startTime,
+        "end_time": endTime,
+        "created_at": createdAt?.toIso8601String(),
+        "updated_at": updatedAt?.toIso8601String(),
+    };
+}
