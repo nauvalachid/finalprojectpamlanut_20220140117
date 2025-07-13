613feb29229a666140f5be36db023439d9af4766
nauvalachid <nauvalachid@gmail.com>
2025-07-14 04:50:17 +0700

    setup dependency injection dan auto-redirect berdasarkan role user

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,161 @@
-import 'package:flutter/material.dart';
+import 'package:finalproject/data/repository/admin_profile_repository.dart';
+import 'package:finalproject/data/repository/auth_repository.dart';
+import 'package:finalproject/data/repository/pasien_repository.dart';
+import 'package:finalproject/data/repository/pendaftaran_repository.dart';
+import 'package:finalproject/data/repository/jadwal_repository.dart';
+import 'package:finalproject/data/repository/resep_repository.dart';
 
-void main() {
+import 'package:finalproject/presentation/admin/bloc/profile/profile_admin_bloc.dart';
+import 'package:finalproject/presentation/admin/home/admin_home_screen.dart';
+import 'package:finalproject/presentation/admin/bloc/pendaftaran/pendaftaran_bloc.dart';
+import 'package:finalproject/presentation/auth/bloc/login/login_bloc.dart';
+import 'package:finalproject/presentation/admin/bloc/pasien/pasien_bloc.dart';
+import 'package:finalproject/presentation/admin/bloc/jadwal/jadwal_bloc.dart';
+import 'package:finalproject/presentation/admin/bloc/resep/resep_bloc.dart';
+
+import 'package:finalproject/presentation/auth/login_screen.dart';
+import 'package:finalproject/presentation/pasien/home/homescreen_pasien.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:finalproject/service/service_http_client.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'dart:developer';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('id_ID', null);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  Future<String?> _getInitialToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    final authRepository = AuthRepository(httpClient: ServiceHttpClient());
+    return authRepository.getToken();
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Future<String?> _getUserRole() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString('user_role');
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider<ServiceHttpClient>(
+          create: (context) => ServiceHttpClient(),
+        ),
+        RepositoryProvider<AuthRepository>(
+          create: (context) => AuthRepository(
+            httpClient: RepositoryProvider.of<ServiceHttpClient>(context),
+          ),
+        ),
+        RepositoryProvider<PasienRepository>(
+          create: (context) => PasienRepository(
+            RepositoryProvider.of<ServiceHttpClient>(context),
+          ),
+        ),
+        RepositoryProvider<PendaftaranRepository>(
+          create: (context) => PendaftaranRepository(
+            RepositoryProvider.of<ServiceHttpClient>(context),
+          ),
+        ),
+        RepositoryProvider<AdminProfileRepository>(
+          create: (context) => AdminProfileRepository(
+            serviceHttpClient: RepositoryProvider.of<ServiceHttpClient>(context),
+          ),
+        ),
+        RepositoryProvider<ResepRepository>(
+          create: (context) => ResepRepository(
+            RepositoryProvider.of<ServiceHttpClient>(context),
+          ),
+        ),
+        RepositoryProvider<JadwalRepository>(
+          create: (context) => JadwalRepository(
+            RepositoryProvider.of<ServiceHttpClient>(context),
+          ),
+        ),
+      ],
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider<LoginBloc>(
+            create: (context) => LoginBloc(
+              authRepository: RepositoryProvider.of<AuthRepository>(context),
             ),
-          ],
+          ),
+          BlocProvider<PasienBloc>(
+            create: (context) => PasienBloc(
+              pasienRepository: RepositoryProvider.of<PasienRepository>(context),
+            ),
+          ),
+          BlocProvider<PendaftaranBloc>(
+            create: (context) => PendaftaranBloc(
+              RepositoryProvider.of<PendaftaranRepository>(context),
+            ),
+          ),
+          BlocProvider<AdminProfileBloc>(
+            create: (context) => AdminProfileBloc(
+              adminProfileRepository: RepositoryProvider.of<AdminProfileRepository>(context),
+            ),
+          ),
+          BlocProvider<ResepBloc>(
+            create: (context) => ResepBloc(
+              resepRepository: RepositoryProvider.of<ResepRepository>(context),
+            ),
+          ),
+          BlocProvider<JadwalBloc>(
+            create: (context) => JadwalBloc(
+              RepositoryProvider.of<JadwalRepository>(context),
+            ),
+          ),
+        ],
+        child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+          ),
+          home: FutureBuilder<String?>(
+            future: _getInitialToken(),
+            builder: (context, tokenSnapshot) {
+              if (tokenSnapshot.connectionState == ConnectionState.waiting) {
+                return const Scaffold(
+                  body: Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                );
+              } else if (tokenSnapshot.hasData && tokenSnapshot.data != null) {
+                return FutureBuilder<String?>(
+                  future: _getUserRole(),
+                  builder: (context, roleSnapshot) {
+                    if (roleSnapshot.connectionState == ConnectionState.waiting) {
+                      return const Scaffold(
+                        body: Center(child: CircularProgressIndicator()),
+                      );
+                    } else if (roleSnapshot.hasData && roleSnapshot.data == 'pasien') {
+                      log('Main: Mengarahkan ke PasienHomeScreen karena peran: pasien');
+                      return const PasienHomeScreen();
+                    } else if (roleSnapshot.hasData && roleSnapshot.data == 'admin') {
+                      log('Main: Mengarahkan ke AdminHomeScreen karena peran: admin');
+                      return const AdminHomeScreen();
+                    } else {
+                      log('Main: Token ada, tapi peran tidak ditemukan atau tidak valid, mengarahkan ke LoginScreen.');
+                      return const LoginScreen();
+                    }
+                  },
+                );
+              } else {
+                log('Main: Tidak ada token, mengarahkan ke LoginScreen.');
+                return const LoginScreen();
+              }
+            },
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
